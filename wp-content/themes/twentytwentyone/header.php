7f0f96b633a3e99a146adc98555a6cd1d690cec9
Laurent Binder <laurentbinder@gmail.com>
2022-01-24 11:30:46 +0100

    npm hc-offcanvas-nav

--- wp-content/themes/twentytwentyone/header.php
+++ wp-content/themes/twentytwentyone/header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header.
  *
@@ -14,19 +15,43 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
+
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php echo esc_url(get_template_directory_uri()) . '/node_modules/hc-offcanvas-nav/dist/hc-offcanvas-nav.css'; ?>">
+	<script src="<?php echo esc_url(get_template_directory_uri()) . '/node_modules/hc-offcanvas-nav/src/js/hc-offcanvas-nav.js'; ?>"></script>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentytwentyone' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'twentytwentyone'); ?></a>
 
-	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+		<?php get_template_part('template-parts/header/site-header'); ?>
 
-	<div id="content" class="site-content">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
+		<div id="content" class="site-content">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+					<nav id="main-nav">
+						<ul>
+							<li><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li>
+								<a href="#">Services</a>
+								<ul>
+									<li>
+										<a href="#">Hosting</a>
+										<ul>
+											<li><a href="#">Private Server</a></li>
+											<li><a href="#">Managed Hosting</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Domains</a></li>
+									<li><a href="#">Websites</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Contact</a></li>
+						</ul>
+					</nav>
